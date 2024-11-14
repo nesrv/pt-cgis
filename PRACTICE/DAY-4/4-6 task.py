d807012e50364a00997e718176f418f9fb692bd6
ne_srv <ne_srv@mail.ru>
2024-11-14 10:21:00 +0300

    autosave

--- PRACTICE/DAY-4/4-6 task.py
+++ PRACTICE/DAY-4/4-6 task.py
@@ -13,7 +13,25 @@ def check_pallindrom(s):
     return s == s[::-1]
 
 
+# print(check_pallindrom('шалаш'))
+# print(check_pallindrom('АННА'))
+# print(check_pallindrom('Анна'))
 
-print(check_pallindrom('шалаш'))
-print(check_pallindrom('АННА'))
-print(check_pallindrom('Анна'))
+def is_odd(x):
+    return x % 2
+
+
+from string import *
+chars = ascii_lowercase + ascii_uppercase + digits + '_'
+
+
+def check_email(email):
+    if '@' in email and '.' in email:
+        email = email.replace('@','').replace('.','')
+        for char in email:
+            if not char in chars:
+                return "НЕТ"
+        return "ДА"
+    return "НЕТ"
+
+print(check_email('sc@liblistcom'))
