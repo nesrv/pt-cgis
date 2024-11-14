367c49830dcd9bfcfeecbd17e5edc1f702e46ce6
ne_srv <ne_srv@mail.ru>
2024-11-14 10:08:29 +0300

    autosave

--- PRACTICE/DAY-4/4-6 task.py
+++ PRACTICE/DAY-4/4-6 task.py
@@ -3,6 +3,17 @@ def get_html(s, tag='h1'):
     return f'<{tag}> {s} </{tag}>'
 
 
-print(get_html('Hello Python'))
-print(get_html('Hello Python', 'div'))
-print(get_html('Hello Python', tag='p'))
+# print(get_html('Hello Python'))
+# print(get_html('Hello Python', 'div'))
+# print(get_html('Hello Python', tag='p'))
+
+
+def check_pallindrom(s):
+    s = s.upper()
+    return s == s[::-1]
+
+
+
+print(check_pallindrom('шалаш'))
+print(check_pallindrom('АННА'))
+print(check_pallindrom('Анна'))
