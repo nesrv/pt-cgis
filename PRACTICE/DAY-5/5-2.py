c86874be5e2da45fc6794f1748c204e11b14f506
ne_srv <ne_srv@mail.ru>
2024-11-15 09:34:43 +0300

    autosave

---
+++ PRACTICE/DAY-5/5-2.py
@@ -0,0 +1,19 @@
+from time import ctime
+
+a = 0
+
+print("Я к вам пишу – чего же боле?")
+# print(a)
+
+f = open('error-log.txt', 'a', encoding='utf-8')
+
+try:
+    print(b)
+    file = open("myfile2.txt")
+    10 / a
+
+
+
+
+except Exception as Error:
+    print(f"ошибка {Error} в {ctime()}", file=f)
