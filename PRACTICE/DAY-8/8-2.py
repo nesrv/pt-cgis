fc2c49e4ca1723570465db476b31c741ab75bc2d
ne_srv <ne_srv@mail.ru>
2024-11-19 10:29:14 +0300

    autosave

--- PRACTICE/DAY-8/8-2.py
+++ PRACTICE/DAY-8/8-2.py
@@ -1,12 +1,11 @@
 class Lamp:
-    brand = 'Эра'
-    count = 0
+    __slots__ = ('__state', '__floor', 'brand')
 
     def __init__(self, floor=0):
         self.__state = False  # private
         self.__floor = floor
-        Lamp.count += 1
-        self.count = Lamp.count
+        self.brand =  'Эра'
+
 
     @property
     def state(self):
@@ -20,14 +19,6 @@ class Lamp:
             print("Неправильно!")
 
 
-    def switch_on(self):
-        if self.__state == False:
-            self.__state = True
-
-    def switch_off(self):
-        if self.__state == True:
-            self.__state = False
-
     def get_floor(self):
         return self.__floor
 
@@ -37,17 +28,20 @@ class Lamp:
     floor = property(get_floor, set_floor)
 
     def __str__(self):
-        return f'Я лампочка № {self.count} "{Lamp.brand}" на {self.__floor} этаже'
+        return f'Я лампочка "{self.brand}" на {self.__floor} этаже'
 
 
 lamp1 = Lamp(10)
 lamp2 = Lamp(5)
 
 lamp1.floor = 30
-lamp1.state = 'абракадабра'
+# lamp1.color = 'новый цвет'
 lamp1.floor = "цоколь"
+print(lamp1)
+print(lamp2)
 
 
-print(lamp1.floor)
+# print(lamp1.__dict__)
+print(lamp1.__slots__)
 
 
