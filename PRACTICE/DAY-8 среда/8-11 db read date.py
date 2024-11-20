af34e67b5d6dbfff717bcae29f23e6bc1ba6749d
ne_srv <ne_srv@mail.ru>
2024-11-20 12:05:37 +0300

    autosave

--- PRACTICE/DAY-8 среда/8-11 db read date.py
+++ PRACTICE/DAY-8 среда/8-11 db read date.py
@@ -8,7 +8,25 @@ sql3 = '''
 SELECT * FROM people
 '''
 
-cursor.execute(sql3)
+sql4 = '''
+SELECT name, age FROM people
+WHERE name='Павел'
+'''
 
+sql5 = '''
+SELECT name, age FROM people
+WHERE age > 30
+'''
+cursor.execute(sql5)
+
+# lst_users = cursor.fetchall()
+
+# for user in lst_users:
+#     print(user[1], user[2])
+
+# print(cursor.fetchone())
+# print(cursor.fetchone())
+# print(cursor.fetchone())
+
+# print(cursor.fetchmany(3))
 print(cursor.fetchall())
-
